2547dd5fde2f9fa7d61908cc055e619b07037ba2
Carlos Leopoldo <carlos@carlosleopoldo.com>
2024-07-11 11:59:21 -0600

    Instalación de @auth0/angular-jwt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { routes } from './app.routes';
 
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withHashLocation()),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+        },
+      })
+    ),
+  ],
 };

e996338f56972772cae9a6866d2615a135d92dbd
Carlos Leopoldo <carlos@carlosleopoldo.com>
2024-07-11 12:34:39 -0600

    Definición de rutas iniciales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { FavoriteListComponent } from './components/favorite-list/favorite-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'videos',
+    component: VideoListComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'favorites',
+    component: FavoriteListComponent,
+    canActivate: [authGuard],
+  },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' },
+];

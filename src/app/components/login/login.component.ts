c25c7337216a7e4e3026c2560b8a7e8af071eec0
Carlos Leopoldo <carlos@carlosleopoldo.com>
2024-07-11 21:52:43 -0600

    Formulario de login con validaciones

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,16 @@
 import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { Message } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 import { MessagesModule } from 'primeng/messages';
+import { PasswordModule } from 'primeng/password';
 import { Observer, Subscription } from 'rxjs';
 
 import { AuthService } from '../../services/auth.service';
@@ -11,25 +18,36 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterLink, ButtonModule, MessagesModule],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    ButtonModule,
+    MessagesModule,
+    PasswordModule,
+    InputTextModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnDestroy {
-  user: string = '';
-  password: string = '';
-  errorMessage: string = '';
+  loginForm: FormGroup;
   messages: Message[] = [];
   private subscription: Subscription = new Subscription();
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private cdr: ChangeDetectorRef
-  ) {}
+    private cdr: ChangeDetectorRef,
+  ) {
+    this.loginForm = new FormGroup({
+      user: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
 
   login(): void {
-    this.authService.login(this.user, this.password).subscribe({
+    const { user, password } = this.loginForm.value;
+    this.authService.login(user, password).subscribe({
       next: () => {
         // Navegar a la página protegida después de iniciar sesión exitosamente
         this.router.navigate(['/videos']);
@@ -45,8 +63,7 @@ export class LoginComponent implements OnDestroy {
         ];
         this.cdr.detectChanges();
       },
-      complete: () => {
-      },
+      complete: () => {},
     } as Observer<any>);
   }
 

062c4fefca948aae120c6dce222512f8964ff0bd
Carlos Leopoldo <carlos@carlosleopoldo.com>
2024-07-12 23:52:30 -0600

    Diseño de la página de favoritos

--- src/app/components/favorite-list/favorite-list.component.ts
+++ src/app/components/favorite-list/favorite-list.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-favorite-list',
   standalone: true,
-  imports: [],
+  imports: [MenuModule, ButtonModule],
   templateUrl: './favorite-list.component.html',
-  styleUrl: './favorite-list.component.scss'
+  styleUrl: './favorite-list.component.scss',
 })
-export class FavoriteListComponent {
+export class FavoriteListComponent implements OnInit {
+  menuItems: MenuItem[] | undefined;
+  userData: any;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.userData = authService.getUserData();
+  }
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: this.userData.name,
+        items: [
+          {
+            label: 'Favoritos',
+            icon: 'pi pi-heart',
+            command: () => {
+              this.router.navigate(['/favorites']);
+            },
+          },
+        ],
+      },
+      {
+        label: 'Sesión',
+        items: [
+          {
+            label: 'Cerrar sesión',
+            icon: 'pi pi-sign-out',
+            command: () => {
+              this.signout();
+            },
+          },
+        ],
+      },
+    ];
+  }
+
+  public signout() {
+    this.authService.logout();
+  }
 }

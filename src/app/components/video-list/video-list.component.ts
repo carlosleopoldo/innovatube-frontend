5769463633fe57accc3d2d94635260aa8a01837f
Carlos Leopoldo <carlos@carlosleopoldo.com>
2024-07-12 23:38:09 -0600

    Encabezado del sitio, con menu, buscador y logo

--- src/app/components/video-list/video-list.component.ts
+++ src/app/components/video-list/video-list.component.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-video-list',
   standalone: true,
-  imports: [],
+  imports: [MenuModule, ButtonModule],
   templateUrl: './video-list.component.html',
-  styleUrl: './video-list.component.scss'
+  styleUrl: './video-list.component.scss',
 })
-export class VideoListComponent {
+export class VideoListComponent implements OnInit, AfterViewInit {
+  menuItems: MenuItem[] | undefined;
+  userData: any;
+  isInputFocused: boolean = false;
+  @ViewChild('searchInput', { static: true })
+  searchInput!: ElementRef<HTMLInputElement>;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.userData = authService.getUserData();
+  }
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: this.userData.name,
+        items: [
+          {
+            label: 'Favoritos',
+            icon: 'pi pi-heart',
+            command: () => {
+              this.router.navigate(['/favorites']);
+            },
+          },
+        ],
+      },
+      {
+        label: 'Sesión',
+        items: [
+          {
+            label: 'Cerrar sesión',
+            icon: 'pi pi-sign-out',
+            command: () => {
+              this.signout();
+            },
+          },
+        ],
+      },
+    ];
+  }
+
+  ngAfterViewInit() {
+    this.focusInput();
+  }
+
+  public focusInput(): void {
+    if (this.searchInput) {
+      this.searchInput.nativeElement.focus();
+    }
+  }
+
+  public onInputFocus() {
+    this.isInputFocused = true;
+  }
+
+  public onInputBlur() {
+    this.isInputFocused = false;
+  }
+
+  public signout() {
+    this.authService.logout();
+  }
 }
